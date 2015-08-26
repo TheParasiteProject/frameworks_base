35438a8fe3d782b7cb8d1756148c954ca5feef51
d34d <clark@cyngn.com>
2015-08-25 19:13:46 -0700

    SystemUI: Show icon of package associated with Toast
    For all those times you have some random app or background service that posts
    a Toast and you have no idea who's posting it.  This adds an icon badge to the
    top left corner of the Toast to show the app's icon the Toast belongs to
    
    Change-Id: I82bf23664eea134f3b1f89ad5a99f6be73906ba8
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- core/java/android/widget/ToastPresenter.java
+++ core/java/android/widget/ToastPresenter.java
@@ -22,8 +22,10 @@ import android.annotation.Nullable;
 import android.app.INotificationManager;
 import android.app.ITransientNotificationCallback;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.os.IBinder;
@@ -251,6 +253,22 @@ public class ToastPresenter {
 
         adjustLayoutParams(mParams, windowToken, duration, gravity, xOffset, yOffset,
                 horizontalMargin, verticalMargin, removeWindowAnimations);
+
+        ImageView appIcon = (ImageView) mView.findViewById(android.R.id.icon);
+        if (appIcon != null) {
+            Context context = mContext.get();
+            if (context != null) {
+                PackageManager pm = context.getPackageManager();
+                Drawable icon = null;
+                try {
+                    icon = pm.getApplicationIcon(mPackageName);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // nothing to do
+                }
+                appIcon.setImageDrawable(icon);
+            }
+        }
+
         addToastView();
         trySendAccessibilityEvent(mView, mPackageName);
         if (callback != null) {
