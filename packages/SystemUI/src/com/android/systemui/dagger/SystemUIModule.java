97303b7ae96b28e50d2fed555ac8733922e26b50
Keith Mok <kmok@cyngn.com>
2016-02-04 11:10:22 -0800

    SystemUI: Add caffeine QS tile
    Co-authored-by: chillger <alexander.loskutov@gmail.com>
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I0b9a498285930cc397b7c4d20ddec3f447e6d332

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUIModule.java
@@ -62,6 +62,7 @@ import com.android.systemui.keyboard.KeyboardModule;
 import com.android.systemui.keyevent.data.repository.KeyEventRepositoryModule;
 import com.android.systemui.keyguard.ui.view.layout.blueprints.KeyguardBlueprintModule;
 import com.android.systemui.keyguard.ui.view.layout.sections.KeyguardSectionsModule;
+import com.android.systemui.lineage.LineageModule;
 import com.android.systemui.log.dagger.LogModule;
 import com.android.systemui.log.dagger.MonitorLog;
 import com.android.systemui.log.table.TableLogBuffer;
@@ -198,6 +199,7 @@ import javax.inject.Named;
         KeyguardBlueprintModule.class,
         KeyguardSectionsModule.class,
         LetterboxModule.class,
+        LineageModule.class,
         LogModule.class,
         MediaProjectionModule.class,
         MediaProjectionTaskSwitcherModule.class,
