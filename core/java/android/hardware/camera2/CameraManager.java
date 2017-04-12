f72c8d2f93380cf5545e6a5f71869fbe059a7357
Gaoxiang Chen <gaochen@codeaurora.org>
2017-04-12 20:05:24 +0800

    Camera: Ignore torch status update for aux or compsite camera
    Issue:
      We only expose aux and composite camera ids to whitelist app,
      but don't check the whitelist in torch status update.
      So for apps not in whitelist like CTS,
      they might receive extra unexpected torch status update,
      from aux and composite camera, which could cause CTS failure.
    
    Fix:
      Also ignore torch status update, if app not in whitelist.
      It can fixe CTS issues in FlashlightTest.java.
    
    CRs-Fixed: 2033688
    Change-Id: I308bccfc027017b590f9f91089d644f7d8a206ff
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- core/java/android/hardware/camera2/CameraManager.java
+++ core/java/android/hardware/camera2/CameraManager.java
@@ -37,6 +37,7 @@ import android.compat.annotation.ChangeId;
 import android.compat.annotation.EnabledSince;
 import android.compat.annotation.Overridable;
 import android.content.AttributionSourceState;
+import android.app.ActivityThread;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
@@ -70,6 +71,7 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.ServiceSpecificException;
 import android.os.SystemProperties;
+import android.text.TextUtils;
 import android.util.ArrayMap;
 import android.util.ArraySet;
 import android.util.Log;
@@ -2665,6 +2667,26 @@ public final class CameraManager {
                     throw new IllegalArgumentException("cameraId was null");
                 }
 
+                /* Force to expose only two cameras
+                 * if the package name does not falls in this bucket
+                 */
+                boolean exposeAuxCamera = false;
+                String packageName = ActivityThread.currentOpPackageName();
+                String packageList = SystemProperties.get("vendor.camera.aux.packagelist");
+                if (packageList.length() > 0) {
+                    TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+                    splitter.setString(packageList);
+                    for (String str : splitter) {
+                        if (packageName.equals(str)) {
+                            exposeAuxCamera = true;
+                            break;
+                        }
+                    }
+                }
+                if (exposeAuxCamera == false && (Integer.parseInt(cameraId) >= 2)) {
+                    throw new IllegalArgumentException("invalid cameraId");
+                }
+
                 ICameraService cameraService = getCameraService();
                 if (cameraService == null) {
                     throw new CameraAccessException(CameraAccessException.CAMERA_DISCONNECTED,
@@ -3087,6 +3109,31 @@ public final class CameraManager {
                         "Camera id %s has torch status changed to 0x%x for device %d",
                         info.mCameraId, status, info.mDeviceId));
             }
+
+            /* Force to ignore the aux or composite camera torch status update
+             * if the package name does not falls in this bucket
+             */
+            boolean exposeMonoCamera = false;
+            String packageName = ActivityThread.currentOpPackageName();
+            String packageList = SystemProperties.get("vendor.camera.aux.packagelist");
+            if (packageList.length() > 0) {
+                TextUtils.StringSplitter splitter = new TextUtils.SimpleStringSplitter(',');
+                splitter.setString(packageList);
+                for (String str : splitter) {
+                    if (packageName.equals(str)) {
+                        exposeMonoCamera = true;
+                        break;
+                    }
+                }
+            }
+
+            if (exposeMonoCamera == false) {
+                if (Integer.parseInt(info.mCameraId) >= 2) {
+                    Log.w(TAG, "ignore the torch status update of camera: " + info.mCameraId);
+                    return;
+                }
+            }
+
 
             if (!validTorchStatus(status)) {
                 Log.e(TAG, String.format(
