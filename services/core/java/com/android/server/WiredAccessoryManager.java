fbd3d8cc292442042dae6f059284af1c8cf4c0f7
Dhananjay Kumar <dhakumar@codeaurora.org>
2019-04-30 01:14:58 +0530

    audio: update WiredAccessoryManager to use ExtconUEventObserver based on events available
    Update WiredAccessoryManager to use ExtconUEventObserver only if
    at least one event is available with ExtconUEventObserver
    
    CRs-Fixed: 2444687
    Change-Id: I457630428a06016ab71c797efd887f55fcba70c6

--- services/core/java/com/android/server/WiredAccessoryManager.java
+++ services/core/java/com/android/server/WiredAccessoryManager.java
@@ -149,7 +149,7 @@ final class WiredAccessoryManager implements WiredAccessoryCallbacks {
             }
         }
 
-        if (ExtconUEventObserver.extconExists()) {
+        if (ExtconUEventObserver.extconExists() && mExtconObserver.uEventCount() > 0) {
             if (mUseDevInputEventForAudioJack) {
                 Log.w(TAG, "Both input event and extcon are used for audio jack,"
                         + " please just choose one.");
@@ -762,6 +762,10 @@ final class WiredAccessoryManager implements WiredAccessoryCallbacks {
                 startObserving(extconInfo);
             }
 
+        }
+
+        public int uEventCount() {
+            return mExtconInfos.size();
         }
 
         @Override
