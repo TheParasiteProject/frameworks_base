932fb4f8d25a88bf2adac83b1982c6b938f73f3e
Jyotiraditya Panda <jyotiraditya@aospa.co>
2021-02-27 20:32:26 +0530

    base: Redo Pocket mode view like OOS
    Change-Id: I037f5120315581cade60b6c11a64b07cc47ba376
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/pocket/PocketLock.java
+++ services/core/java/com/android/server/policy/pocket/PocketLock.java
@@ -53,7 +53,7 @@ public class PocketLock {
         mWindowManager = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
         mLayoutParams = getLayoutParams();
         mView = LayoutInflater.from(mContext).inflate(
-                org.lineageos.platform.internal.R.layout.pocket_lock_view_layout, null);
+                org.lineageos.platform.internal.R.layout.pocket_lock_view, null);
     }
 
     public void show(final boolean animate) {
