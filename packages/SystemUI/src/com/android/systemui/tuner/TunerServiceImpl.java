43715b8e4823931f4776a33220b54ac7a54aa62f
Joe Maples <joe@frap129.org>
2021-10-19 19:23:01 +0000

    Tuner: Don't clear out prefs, it's not disabled
    Change-Id: I7bea54fa19c61f3c769c0bafd9ea359ad1bd3356
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -159,13 +159,6 @@ public class TunerServiceImpl extends TunerService {
         if (oldVersion < 2) {
             setTunerEnabled(false);
         }
-        // 3 Removed because of a revert.
-        if (oldVersion < 4) {
-            // Delay this so that we can wait for everything to be registered first.
-            final int user = mCurrentUser;
-            mainHandler.postDelayed(
-                    () -> clearAllFromUser(user), 5000);
-        }
         setValue(TUNER_VERSION, newVersion);
     }
 
@@ -381,6 +374,9 @@ public class TunerServiceImpl extends TunerService {
 
     private void reloadAll() {
         for (String key : mTunableLookup.keySet()) {
+            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key) || isLineageSetting(key)) {
+                continue;
+            }
             String value = getValue(key);
             for (Tunable tunable : mTunableLookup.get(key)) {
                 if (tunable != null) {
@@ -402,9 +398,6 @@ public class TunerServiceImpl extends TunerService {
 
         // A couple special cases.
         for (String key : mTunableLookup.keySet()) {
-            if (ArrayUtils.contains(RESET_EXCEPTION_LIST, key) || isLineageSetting(key)) {
-                continue;
-            }
             setValue(key, null);
         }
     }
