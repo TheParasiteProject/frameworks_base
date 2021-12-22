44556a70a407823797fefb83b2510baf2c4d9eb8
Danny Lin <danny@kdrag0n.dev>
2021-12-21 17:32:33 -0800

    AppErrors: Respect appsNotReportingCrashes for ANRs
    We want to use config_appsNotReportingCrashes to silence benign System
    UI ANRs seemingly caused by an AOSP bug related to freezing the
    screenshot process, until Google fixes it:
    
    E ActivityManager: ANR in com.android.systemui:screenshot
    E ActivityManager: Reason: Input dispatching timed out (Screenshot (server) is not responding. Waited 5002ms for MotionEvent)
    E ActivityManager: Frozen: true
    
    Change-Id: I213c1d99d35fb80016e5a72136c99b5fb2cde4a6

--- services/core/java/com/android/server/am/AppErrors.java
+++ services/core/java/com/android/server/am/AppErrors.java
@@ -1127,9 +1127,12 @@ class AppErrors {
             }
 
             int visibleUserId = getVisibleUserId(proc.userId);
+            final boolean anrSilenced = mAppsNotReportingCrashes != null
+                    && mAppsNotReportingCrashes.contains(proc.info.packageName);
             boolean showBackground = Settings.Secure.getIntForUser(mContext.getContentResolver(),
                     Settings.Secure.ANR_SHOW_BACKGROUND, 0, visibleUserId) != 0;
-            if (mService.mAtmInternal.canShowErrorDialogs(visibleUserId) || showBackground) {
+            if (!anrSilenced &&
+                    (mService.mAtmInternal.canShowErrorDialogs(visibleUserId) || showBackground)) {
                 AnrController anrController = errState.getDialogController().getAnrController();
                 if (anrController == null) {
                     errState.getDialogController().showAnrDialogs(data);
@@ -1154,7 +1157,7 @@ class AppErrors {
                 MetricsLogger.action(mContext, MetricsProto.MetricsEvent.ACTION_APP_ANR,
                         AppNotRespondingDialog.CANT_SHOW);
                 // Just kill the app if there is no dialog to be shown.
-                doKill = true;
+                doKill = !anrSilenced;
             }
         }
         if (doKill) {
