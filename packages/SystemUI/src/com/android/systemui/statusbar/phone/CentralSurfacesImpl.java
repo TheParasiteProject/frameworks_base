414ca7291494412df715438661ebd8548db7cf6c
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-27 17:05:57 +0700

    SystemUI: Add support for GameSpace
    This is an addon for GameSpace for broadcasting about game start/stop.
    Additionally, it also has special option for suppressing fullscreen
    intent like incoming call.
    
    This also squash commits:
    - SystemUI: Improve GameSpace lifecycle and broadcast handling
    - GameSpaceManager: Handle various case against "locking screen" scenario
    - GameSpace: Fix issue with multiple users [neobuddy89]
    - SystemUI: Update gamespace for A13 [Genkzsz11]
    - GameSpaceManager: restrict broadcast to MANAGE_GAME_MODE holders only
    - GameSpaceManager: Update TaskStack listener usage
    - GameSpaceManager: Updated for A14 QPR1 [neobuddy89]
    
    Change-Id: If9985af54530d8a81c907ab8a67b1e843fc058ab
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -228,6 +228,7 @@ import com.android.systemui.statusbar.policy.ConfigurationController.Configurati
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.statusbar.policy.ExtensionController;
+import com.android.systemui.statusbar.policy.GameSpaceManager;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
 import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
@@ -486,6 +487,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
     private final UserTracker mUserTracker;
     private final Provider<FingerprintManager> mFingerprintManager;
     private final ActivityStarter mActivityStarter;
+
+    private GameSpaceManager mGameSpaceManager;
 
     private final DisplayMetrics mDisplayMetrics;
 
@@ -864,6 +867,8 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
 
         mActivityIntentHelper = new ActivityIntentHelper(mContext);
         mActivityTransitionAnimator = activityTransitionAnimator;
+
+        mGameSpaceManager = new GameSpaceManager(mContext, mKeyguardStateController);
 
         // TODO(b/190746471): Find a better home for this.
         DateTimeView.setReceiverHandler(timeTickHandler);
@@ -1479,6 +1484,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_SCREEN_CAMERA_GESTURE);
         mBroadcastDispatcher.registerReceiver(mBroadcastReceiver, filter, null, UserHandle.ALL);
+        mGameSpaceManager.observe();
     }
 
     @Override
@@ -1490,6 +1496,11 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces, Tune
         if (getNavigationBarView() != null) {
             getNavigationBarView().setBlockedGesturalNavigation(blocked, mSysUiState);
         }
+    }
+
+    @Override
+    public GameSpaceManager getGameSpaceManager() {
+        return mGameSpaceManager;
     }
 
     protected QS createDefaultQSFragment() {
