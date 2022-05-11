82d8c5a571a44863c277d820e999d33cb0ed61df
Pranav Vashi <neobuddy89@gmail.com>
2022-05-11 22:17:22 +0530

    Add various system packages to indicator exemptions
    * We do not use config_locationProviderPackageNames since DefaultPermissionGrantPolicy
      will give fixed location permissions to them by default.
    * We do not use config_locationExtraPackageNames since these are meant to be used
      by device trees.
    
    Change-Id: I67ba4bf8c56d910330146c9fc349dcaf6c3ef7fb

--- core/java/android/permission/PermissionManager.java
+++ core/java/android/permission/PermissionManager.java
@@ -267,6 +267,7 @@ public final class PermissionManager {
 
     private static String[] sLocationProviderPkgNames;
     private static String[] sLocationExtraPkgNames;
+    private static String[] sExemptedPkgNames;
 
     /**
      * Creates a new instance.
@@ -1381,6 +1382,11 @@ public final class PermissionManager {
                 pkgNames.add(pkgName);
             }
         }
+        for (String pkgName: sExemptedPkgNames) {
+            if (pkgName != null) {
+                pkgNames.add(pkgName);
+            }
+        }
         return pkgNames;
     }
 
@@ -1400,6 +1406,8 @@ public final class PermissionManager {
                     R.array.config_locationProviderPackageNames);
             sLocationExtraPkgNames = context.getResources().getStringArray(
                     R.array.config_locationExtraPackageNames);
+            sExemptedPkgNames = context.getResources().getStringArray(
+                    org.lineageos.platform.internal.R.array.config_indicatorExemptedPackageNames);
         }
     }
     /**
