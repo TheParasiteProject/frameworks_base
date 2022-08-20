3d420a334918f55f4b004d67965c26fd9f15ae42
Arian <arian.kulmer@web.de>
2022-08-20 23:38:25 +0200

    udfps: Restore illumination dot for global hbm
    UdfpsSurfaceView.java is imported from android-12.1.0_r22
    
    Change-Id: I4a4e85a7437a9a444a4f952fd62e4fe12f56ce5a

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsControllerOverlay.kt
@@ -217,6 +217,7 @@ constructor(
                         (inflater.inflate(R.layout.udfps_touch_overlay, null, false)
                                 as UdfpsTouchOverlay)
                             .apply {
+                                setUdfpsDisplayModeProvider(udfpsDisplayModeProvider)
                                 // This view overlaps the sensor area
                                 // prevent it from being selectable during a11y
                                 if (requestReason.isImportantForAccessibility()) {
@@ -238,6 +239,7 @@ constructor(
                                             udfpsOverlayInteractor = udfpsOverlayInteractor,
                                         )
                                 }
+                                sensorRect = sensorBounds
                             }
                 } else {
                     overlayViewLegacy =
@@ -424,6 +426,11 @@ constructor(
                 unconfigureDisplay()
             }
             animationViewController = null
+        }
+        overlayTouchView?.apply {
+            if (isDisplayConfigured) {
+                unconfigureDisplay()
+            }
         }
         if (DeviceEntryUdfpsRefactor.isEnabled) {
             udfpsDisplayModeProvider.disable(null)
