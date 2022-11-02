e7dc74fe129670d69b925c5746d4203b5ca711b7
LuK1337 <priv.luk@gmail.com>
2022-11-02 12:59:32 +0100

    camera: Allow extending aux list/excludelist via resources
    Properties are limited and don't allow us to list as many apps as we
    wish.
    
    Change-Id: I4432f627f1fa120b6ea41c252cd512beb129848c

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -37,6 +37,7 @@ import android.compat.annotation.UnsupportedAppUsage;
 import android.content.AttributionSource.ScopedParcelState;
 import android.content.Context;
 import android.content.res.CameraCompatibilityInfo;
+import android.content.res.Resources;
 import android.graphics.ImageFormat;
 import android.graphics.Point;
 import android.graphics.Rect;
@@ -297,12 +298,20 @@ public class Camera {
         String packageName = ActivityThread.currentOpPackageName();
         if (packageName == null)
             return true;
-        List<String> packageList = Arrays.asList(
-                SystemProperties.get("vendor.camera.aux.packagelist", packageName).split(","));
-        List<String> packageExcludelist = Arrays.asList(
-                SystemProperties.get("vendor.camera.aux.packageexcludelist", "").split(","));
+        List<String> packageList = new ArrayList<>(Arrays.asList(
+                SystemProperties.get("vendor.camera.aux.packagelist", packageName).split(",")));
+        List<String> packageExcludelist = new ArrayList<>(Arrays.asList(
+                SystemProperties.get("vendor.camera.aux.packageexcludelist", "").split(",")));
 
-        return packageList.contains(packageName) && !packageExcludelist.contains(packageName);
+        // Append packages from resources
+        Resources res = ActivityThread.currentApplication().getResources();
+        packageList.addAll(Arrays.asList(res.getStringArray(
+                org.lineageos.platform.internal.R.array.config_cameraAuxPackageAllowList)));
+        packageExcludelist.addAll(Arrays.asList(res.getStringArray(
+                org.lineageos.platform.internal.R.array.config_cameraAuxPackageExcludeList)));
+
+        return (packageList.isEmpty() || packageList.contains(packageName)) &&
+                !packageExcludelist.contains(packageName);
     }
 
     /**
