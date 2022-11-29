a88e43437f5c7d2cb24c9cfff658e28b47582556
Blaster4385 <venkateshchaturvedi12@gmail.com>
2022-11-29 23:33:19 +0530

    base: Add optional haptic feedback to volume panel [1/2]
    [someone5678] Unify style with Brightness slider haptic
    
    Change-Id: Ieb2dc23eb1e8237b230591066901acbb19c94fd6
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6694,6 +6694,13 @@ public final class Settings {
         public static final String QS_TRANSPARENCY = "qs_transparency";
 
         /**
+         * Give haptic feedback on volume slider changes.
+         * @hide
+         */
+        @Readable
+        public static final String VOLUME_SLIDER_HAPTIC_FEEDBACK = "volume_slider_haptic_feedback";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
