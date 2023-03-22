d01b643e11963b296e1830e841ca669c76f6200d
Arthur Shuai <quic_shuaiq@quicinc.com>
2023-03-22 22:12:32 +0800

    base: add support of the Phone Link
    Change-Id: I86680bc723c03f305f66bd5c5b0f110517a97504
    CRs-Fixed: 3408565
    
    base: the second update for Phone Link
    
    Change-Id: Id44b6439614ca50a81157409d28de8cf0fff52a7
    CRs-Fixed: 3408565
    
    Base: define for DISABLE_DEVICE_INTEGRATION
    
    Change-Id: Ib9b22fe41ffd6b3efa1c886c57056a13b2809eee
    CRs-Fixed: 3449717
    
    Delete SystemApi annotation
    
    Change-Id: I9e162cb9db6339268834d47e31a725e21453e86d
    CRs-Fixed：3505958
    
    Update PhoneLink 14-3 frameworks/base change
    
    Change-Id: I4f657a338d127a2d7b0b5ef2469e171e687d2d7b
    CRs-Fixed：3498734
    
    Change-Id: I2fc3aa61bfdaf96a7fab87cf32d170cda5160c1a

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -162,6 +162,7 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Debug;
+import android.os.DeviceIntegrationUtils;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Looper;
@@ -1126,6 +1127,8 @@ public final class ViewRootImpl implements ViewParent,
     // The latest input event from the gesture that was used to resolve the pointer icon.
     private MotionEvent mPointerIconEvent = null;
 
+    private final RemoteTaskWindowInsetHelper mRTWindowInsetHelper;
+
     public ViewRootImpl(Context context, Display display) {
         this(context, display, WindowManagerGlobal.getWindowSession(), new WindowLayout());
     }
@@ -1215,6 +1218,12 @@ public final class ViewRootImpl implements ViewParent,
             }
         } else {
             mSensitiveContentProtectionService = null;
+        }
+        if (!DeviceIntegrationUtils.DISABLE_DEVICE_INTEGRATION) {
+            mRTWindowInsetHelper = new RemoteTaskWindowInsetHelper(context);
+            mInsetsController.getState().setRTWindowInsetHelper(mRTWindowInsetHelper);
+        } else {
+            mRTWindowInsetHelper = null;
         }
     }
 
@@ -2212,6 +2221,11 @@ public final class ViewRootImpl implements ViewParent,
     public void onMovedToDisplay(int displayId, Configuration config) {
         if (mDisplay.getDisplayId() == displayId) {
             return;
+        }
+
+        if (!DeviceIntegrationUtils.DISABLE_DEVICE_INTEGRATION
+            && mRTWindowInsetHelper != null) {
+            mRTWindowInsetHelper.updateDisplayId(displayId);
         }
 
         // Get new instance of display based on current display adjustments. It may be updated later
@@ -11210,6 +11224,17 @@ public final class ViewRootImpl implements ViewParent,
             final ViewRootImpl viewAncestor = mViewAncestor.get();
             if (viewAncestor != null) {
                 viewAncestor.dispatchScrollCaptureRequest(listener);
+            }
+        }
+
+        @Override
+        public void dispatchBlackScreenKeyEvent(KeyEvent event) {
+            final ViewRootImpl viewAncestor = mViewAncestor.get();
+            if (viewAncestor != null) {
+                final View view = viewAncestor.mView;
+                if (view != null) {
+                    view.dispatchKeyEvent(event);
+                }
             }
         }
     }
