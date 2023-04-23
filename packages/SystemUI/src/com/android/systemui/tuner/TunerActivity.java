f343df517c3ec0b34fad9a14babe3bfd91872f88
nift4 <nift4@protonmail.com>
2023-04-23 21:47:42 +0200

    SystemUI: fix antiquated TunerActivity bugs
    Change-Id: Ic99c951e90b10941124a8afef383f4016bff2bed
    Signed-off-by: Farzin <itisFarzin@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerActivity.java
@@ -29,6 +29,8 @@ import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceScreen;
 
+import java.util.ArrayDeque;
+
 import com.android.systemui.Dependency;
 import com.android.systemui.demomode.DemoModeController;
 import com.android.systemui.fragments.FragmentService;
@@ -46,6 +48,7 @@ public class TunerActivity extends Activity implements
     private final DemoModeController mDemoModeController;
     private final TunerService mTunerService;
     private final GlobalSettings mGlobalSettings;
+    private final ArrayDeque<String> titleStack = new ArrayDeque<>();
 
     @Inject
     TunerActivity(
@@ -99,7 +102,22 @@ public class TunerActivity extends Activity implements
 
     @Override
     public void onBackPressed() {
-        if (!getFragmentManager().popBackStackImmediate()) {
+        if (getFragmentManager().popBackStackImmediate()) {
+            String title = titleStack.poll();
+            if (title != null) {
+                setTitle(title);
+            }
+            try {
+                Fragment f = getFragmentManager().findFragmentById(R.id.content_frame);
+                Fragment fragment = (Fragment) f.getClass().newInstance();
+                fragment.setArguments(f.getArguments());
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.content_frame, fragment);
+                transaction.commit();
+            } catch (InstantiationException | IllegalAccessException e) {
+                Log.d("TunerActivity", "Problem launching fragment", e);
+            }
+        } else {
             super.onBackPressed();
         }
     }
@@ -113,6 +131,7 @@ public class TunerActivity extends Activity implements
             b.putString(PreferenceFragment.ARG_PREFERENCE_ROOT, pref.getKey());
             fragment.setArguments(b);
             FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            titleStack.push(getTitle().toString());
             setTitle(pref.getTitle());
             transaction.replace(R.id.content_frame, fragment);
             transaction.addToBackStack("PreferenceFragment");
