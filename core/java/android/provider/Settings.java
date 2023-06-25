a69a28d8cf89d661d73687e7ada95073b041ae26
someone5678 <nemui3353@gmail.com>
2023-06-26 05:57:42 +0900

    base: Guard brightness slider haptic by setting [1/2]
    Change-Id: Ic42c037da8d7cc8378f084ed20205fafc52b95e5
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6052,6 +6052,12 @@ public final class Settings {
         public static final String CLICK_PARTIAL_SCREENSHOT = "click_partial_screenshot";
 
         /**
+         * Give haptic feedback on brightness slider changes.
+         * @hide
+         */
+        public static final String BRIGHTNESS_SLIDER_HAPTIC_ENABLED = "brightness_slider_haptic_enabled";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
