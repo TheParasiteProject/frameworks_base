cb073f0c204a14ef35ccef98f55ef70b4c9bff16
aswin7469 <aswinas@pixysos.com>
2023-07-12 14:29:30 +0530

    PixelPropUtils: refactor
    * Separate GamesProps into GamesPropsUtils
    * Switch from POCO F4 to POCO F5
    * Switch from redfin to barbet
    
    Co-authored-by: someone5678 <nemui3353@gmail.com>
    Co-authored-by: Gustavo Mendes <gusttavo.me@outlook.com>
    Co-authored-by: Joey Huab <joey@evolution-x.org>
    
    Change-Id: Ibdcb82d9282255cdb7c83643ecb59db9768845bf
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    
    * Adapt to current project
    
    Change-Id: I9710d6d59531f4933b0ff68e276cabd789d59db7
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -65,6 +65,7 @@ import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.custom.PixelPropsUtils;
+import com.android.internal.util.custom.GamesPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1246,9 +1247,10 @@ public class Instrumentation {
         app.attach(context);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(packageName);
         return app;
     }
-    
+
     /**
      * Perform instantiation of the process's {@link Application} object.  The
      * default implementation provides the normal system behavior.
@@ -1265,6 +1267,7 @@ public class Instrumentation {
         app.attach(context);
         String packageName = context.getPackageName();
         PixelPropsUtils.setProps(packageName);
+        GamesPropsUtils.setProps(packageName);
         return app;
     }
 
