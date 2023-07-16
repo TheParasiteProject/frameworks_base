b30bf15552d53f10d10f178893db3bff322570cf
Pranav Vashi <neobuddy89@gmail.com>
2023-07-16 20:27:27 +0900

    fixup! Add toggle to enable brightness slider haptic feedback [1/2]
    Change-Id: I7b5ffcf8a44427a33f0f33bebee294cd47007afe
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6382,10 +6382,11 @@ public final class Settings {
         public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
 
         /**
-         * Give haptic feedback on brightness slider changes.
+         * Give haptic feedback on qs brightness slider changes.
          * @hide
          */
-        public static final String BRIGHTNESS_SLIDER_HAPTIC_ENABLED = "brightness_slider_haptic_enabled";
+        @Readable
+        public static final String QS_BRIGHTNESS_SLIDER_HAPTIC_FEEDBACK = "qs_brightness_slider_haptic_feedback";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
