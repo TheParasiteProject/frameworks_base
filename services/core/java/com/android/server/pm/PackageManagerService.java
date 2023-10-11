a6e12fa4c2b53a1dfd62ca04bc1ff13efd4c62f0
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-11 11:27:52 +0800

    service: Do not ensure system package for required services extension
    * there were some instances where ensureSystemPackageName tries to load ext services when modules loading were still being loaded or about to be loaded, this causes ensureSystemPackageName to return nothing, which results to bootloop.
    
    test: define com.google.ext.services via config, m, boots, confirmed that google ext services components works
    Change-Id: I27380f96d48bff7d3996d11bffa09527605fb403
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -2726,8 +2726,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                     "Required services extension package failed due to "
                             + "config_servicesExtensionPackage is empty.");
         }
-        String servicesExtensionPackage = ensureSystemPackageName(computer,
-                configServicesExtensionPackage);
+        String servicesExtensionPackage = configServicesExtensionPackage;
         if (TextUtils.isEmpty(servicesExtensionPackage)) {
             throw new RuntimeException(
                     "Required services extension package is missing, "
