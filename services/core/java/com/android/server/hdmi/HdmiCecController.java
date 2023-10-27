9a6080053ebdd82c2b5c8c1c83d07eb2991c305a
Thomas Makin <halorocker89@gmail.com>
2023-10-27 00:31:42 -0500

    CEC: correct frameworks physicalAddress parsing
    The HIDL interface defines physical address as a uint16_t, but Java
    does not utilize unsigned types, so it represents this value as a
    standard 32-bit signed integer type. However, it extends the MSB to
    the full 32-bits instead of padding with zeroes, causing large
    values to be read as negative, crashing the service when
    HdmiPortInfo builder asserts >=0.
    
    This also clarifies a related comment likely sourced from the same
    incorrect assumption about how the parsing behaves.
    
    Co-authored-by: Thomas Makin <halorocker89@gmail.com>
    Co-authored-by: Aaron Kling <webgeek1234@gmail.com>
    
    Change-Id: If101cf5a725315d75ea5315d4cf42c8d55fb7bec

--- services/core/java/com/android/server/hdmi/HdmiCecController.java
+++ services/core/java/com/android/server/hdmi/HdmiCecController.java
@@ -350,7 +350,7 @@ final class HdmiCecController {
      * {@link HdmiCecNetwork} only.
      *
      * @return CEC physical address of the device. The range of success address
-     *         is between 0x0000 and 0xFFFF. If failed it returns -1
+     *         is between 0x0000 and 0xFFFE. If failed it returns INVALID_PHYSICAL_ADDRESS.
      */
     @ServiceThreadOnly
     int getPhysicalAddress() {
@@ -1123,7 +1123,7 @@ final class HdmiCecController {
                     hdmiPortInfo[i] = new HdmiPortInfo.Builder(
                                     portInfo.portId,
                                     portInfo.type,
-                                    portInfo.physicalAddress)
+                                    Short.toUnsignedInt((short)portInfo.physicalAddress))
                                     .setCecSupported(portInfo.cecSupported)
                                     .setMhlSupported(false)
                                     .setArcSupported(portInfo.arcSupported)
@@ -1313,7 +1313,7 @@ final class HdmiCecController {
                     hdmiPortInfo[i] = new HdmiPortInfo.Builder(
                             portInfo.portId,
                             portInfo.type,
-                            portInfo.physicalAddress)
+                            Short.toUnsignedInt(portInfo.physicalAddress))
                             .setCecSupported(portInfo.cecSupported)
                             .setMhlSupported(false)
                             .setArcSupported(portInfo.arcSupported)
@@ -1510,7 +1510,7 @@ final class HdmiCecController {
                     hdmiPortInfo[i] = new HdmiPortInfo.Builder(
                             portInfo.portId,
                             portInfo.type,
-                            portInfo.physicalAddress)
+                            Short.toUnsignedInt(portInfo.physicalAddress))
                             .setCecSupported(portInfo.cecSupported)
                             .setMhlSupported(false)
                             .setArcSupported(portInfo.arcSupported)
