a6deb515535963f64a471325ba29e76eb2c8b554
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-30 17:42:27 +0800

    TelephonyManager: Gracefully handle null telephony service
    --------- beginning of crash
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: FATAL EXCEPTION: main
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: Process: com.qti.phone, PID: 3878
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: java.lang.RuntimeException: Unable to create service com.qualcomm.qcrilmsgtunnel.QcrilMsgTunnelService: java.lang.IllegalStateException: telephony service is null.
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.app.ActivityThread.handleCreateService(ActivityThread.java:4664)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.app.ActivityThread.-$$Nest$mhandleCreateService(Unknown Source:0)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2264)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:205)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:294)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8177)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:971)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: Caused by: java.lang.IllegalStateException: telephony service is null.
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.telephony.TelephonyManager.getHalVersion(TelephonyManager.java:15216)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.telephony.TelephonyManager.getRadioHalVersion(TelephonyManager.java:15104)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at com.qualcomm.qcrilmsgtunnel.QcrilOemhookMsgTunnel.isAidlHalAvailable(QcrilOemhookMsgTunnel.java:203)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at com.qualcomm.qcrilmsgtunnel.QcrilOemhookMsgTunnel.isAidlAvailable(QcrilOemhookMsgTunnel.java:218)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at com.qualcomm.qcrilmsgtunnel.QcrilOemhookMsgTunnel.<init>(QcrilOemhookMsgTunnel.java:178)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at com.qualcomm.qcrilmsgtunnel.QcrilMsgTunnelIfaceManager.<init>(QcrilMsgTunnelIfaceManager.java:77)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at com.qualcomm.qcrilmsgtunnel.QcrilMsgTunnelService.onCreate(QcrilMsgTunnelService.java:54)
    10-30 12:08:30.637  3878  3878 E AndroidRuntime: 	at android.app.ActivityThread.handleCreateService(ActivityThread.java:4651)
    
    reference:
    https://github.com/minaripenguin/android_frameworks_opt_telephony/blob/4aec0433298e6f5776d9c298cc534603124bf8b4/src/java/com/android/internal/telephony/RILUtils.java#L5191
    https://github.com/minaripenguin/android_frameworks_opt_telephony/blob/4aec0433298e6f5776d9c298cc534603124bf8b4/src/java/com/android/internal/telephony/RadioConfig.java#L63
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -10408,7 +10408,8 @@ public class TelephonyManager {
             if (telephony != null) {
                 return telephony.getAllowedNetworkTypesForReason(getSubId(), reason);
             } else {
-                throw new IllegalStateException("telephony service is null.");
+                Rlog.d(TAG, "telephony service is null.");
+                return -1; // NETWORK_MODE_UNKNOWN
             }
         } catch (RemoteException ex) {
             Rlog.e(TAG, "getAllowedNetworkTypesForReason RemoteException", ex);
@@ -16404,7 +16405,8 @@ public class TelephonyManager {
                     return new Pair<Integer, Integer>(version / 100, version % 100);
                 }
             } else {
-                throw new IllegalStateException("telephony service is null.");
+                Log.d(TAG, "telephony service is null.");
+                return HAL_VERSION_UNKNOWN;
             }
         } catch (RemoteException e) {
             Log.e(TAG, "getHalVersion() RemoteException", e);
