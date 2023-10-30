21eaa62b897526dbe4c4af8eb35058c5090dae14
minaripenguin <minaripenguin@users.noreply.github.com>
2023-10-30 17:47:00 +0800

    ServiceManager: Handle all types of exceptions
    * attempt to mitigate:
    
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: FATAL EXCEPTION: main
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: Process: com.google.android.turboadapter, PID: 18509
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: java.lang.RuntimeException: Unable to bind to service com.google.android.turboadapter.GoogleBatteryService@886a671 with Intent { cmp=com.google.android.turboadapter/.GoogleBatteryService }: java.lang.SecurityException: SELinux denied.
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.app.ActivityThread.handleBindService(ActivityThread.java:4696)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.app.ActivityThread.-$$Nest$mhandleBindService(Unknown Source:0)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:2272)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.Handler.dispatchMessage(Handler.java:106)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.Looper.loopOnce(Looper.java:205)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.Looper.loop(Looper.java:294)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.app.ActivityThread.main(ActivityThread.java:8177)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at java.lang.reflect.Method.invoke(Native Method)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at com.android.internal.os.RuntimeInit$MethodAndArgsCaller.run(RuntimeInit.java:552)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:971)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: Caused by: java.lang.SecurityException: SELinux denied.
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.Parcel.createExceptionOrNull(Parcel.java:3057)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.Parcel.createException(Parcel.java:3041)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:3024)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.Parcel.readException(Parcel.java:2966)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.IServiceManager$Stub$Proxy.isDeclared(IServiceManager.java:542)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.ServiceManagerProxy.isDeclared(ServiceManagerNative.java:90)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.ServiceManager.isDeclared(ServiceManager.java:247)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.os.ServiceManager.waitForDeclaredService(ServiceManager.java:303)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at com.google.android.turboadapter.GoogleBatteryService.getGoogleBatteryHAL(GoogleBatteryService.java:134)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at com.google.android.turboadapter.GoogleBatteryService.initHalInterface(GoogleBatteryService.java:158)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at com.google.android.turboadapter.GoogleBatteryService.onBind(GoogleBatteryService.java:75)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	at android.app.ActivityThread.handleBindService(ActivityThread.java:4681)
    10-30 14:16:03.024 18509 18509 E AndroidRuntime: 	... 9 more
    
    Change-Id: I32ef7e16a00622d19e0bb29cc8c83bad23bf725e
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/os/ServiceManager.java
+++ core/java/android/os/ServiceManager.java
@@ -295,7 +295,7 @@ public final class ServiceManager {
     public static boolean isDeclared(@NonNull String name) {
         try {
             return getIServiceManager().isDeclared(name);
-        } catch (RemoteException | SecurityException e) {
+        } catch (Exception e) {
             Log.e(TAG, "error in isDeclared", e);
             return false;
         }
