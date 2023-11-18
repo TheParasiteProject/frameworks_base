0d825d146a843f2af5ea86fa302ca46ee7f4c22a
minaripenguin <minaripenguin@users.noreply.github.com>
2023-11-19 00:40:47 +0800

    SystemUI: Implement Island notification feature [1/2]
    * slimmed down version of reticker
    
    Co-authored-by: Dil3mm4 <dil3mm4.dev@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: someone5678 <someone5678@users.noreply.github.com>
    Change-Id: Ia93d2f47aeed0e88cb85916b79bcfca0f10eed93
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6325,6 +6325,16 @@ public final class Settings {
         public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION = "island_notification";
+
+        /**
+         * @hide
+         */
+        public static final String ISLAND_NOTIFICATION_NOW_PLAYING = "island_notification_now_playing";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
