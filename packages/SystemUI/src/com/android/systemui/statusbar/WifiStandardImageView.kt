c5bf0878df596d3e4816225ef65e01ab5855fe22
minaripenguin <minaripenguin@users.noreply.github.com>
2023-12-01 15:09:21 +0800

    SystemUI: Introduce wifi standard icon feature
    * icons taken from: a799a35c5527482f2d577783170b0478b8021b3f "base: Refactor Wi-Fi standard icons"
    
    [neobuddy89: Added wifi 7 icon]
    [erfanabdi: Move standard icon to the left side]
    [Dhina17: Avoid usage of Resources#getIdentifier() , Switch to ContentObserver]
    
    Change-Id: I187fc97b9f4ad6d48b6c6f0b1215ffbcb1c972e2
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Dhina17 <dhinalogu@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>

---
+++ packages/SystemUI/src/com/android/systemui/statusbar/WifiStandardImageView.kt
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2023-2024 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.android.systemui.statusbar
+
+import android.content.Context
+import android.content.res.Resources
+import android.provider.Settings
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiManager
+import android.os.UserHandle
+import android.util.AttributeSet
+import android.widget.ImageView
+import com.android.systemui.res.R
+import android.view.ViewGroup.MarginLayoutParams
+import kotlinx.coroutines.*
+
+class WifiStandardImageView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ImageView(context, attrs, defStyleAttr) {
+
+    private val connectivityManager: ConnectivityManager by lazy { context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager }
+    private val wifiManager: WifiManager by lazy { context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager }
+    private var currWifiStandardEnabled = false
+    private var currWifiStandard: Int = -1
+    private var updateJob: Job? = null
+
+    private val updateRunnable = object : Runnable {
+        override fun run() {
+            val wifiStandardEnabled = getWifiStandardEnabled()
+            val wifiStandard = getWifiStandard()
+            if (currWifiStandard != wifiStandard
+                || currWifiStandardEnabled != wifiStandardEnabled) {
+                currWifiStandard = wifiStandard
+                currWifiStandardEnabled = wifiStandardEnabled
+                updateWifiStandard()
+            }
+        }
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        updateJob = CoroutineScope(Dispatchers.Main).launch {
+            while (isAttachedToWindow) {
+                updateRunnable.run()
+                delay(1000)
+            }
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        updateJob?.cancel()
+    }
+
+    private fun getWifiStandard(): Int {
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return if (networkCapabilities != null && networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+            val wifiInfo = wifiManager.connectionInfo
+            wifiInfo.wifiStandard
+        } else {
+            -1
+        }
+    }
+
+    private fun updateWifiStandard() {
+        updateIcon()
+    }
+
+    private fun getWifiStandardEnabled(): Boolean {
+        return Settings.System.getIntForUser(
+            context.contentResolver,
+            Settings.System.WIFI_STANDARD_ICON,
+            0,
+            UserHandle.USER_CURRENT
+        ) == 1
+    }
+
+    private fun updateIcon() {
+        post {
+            if (!currWifiStandardEnabled || currWifiStandard < 4) {
+                visibility = GONE
+                layoutParams = (layoutParams as MarginLayoutParams).apply {
+                    marginEnd = 0
+                }
+            } else {
+                val drawableId = getDrawableForWifiStandard()
+                if (drawableId > 0) {
+                    setImageResource(drawableId)
+                    visibility = VISIBLE
+                    layoutParams = (layoutParams as MarginLayoutParams).apply {
+                        marginEnd = resources.getDimensionPixelSize(R.dimen.status_bar_airplane_spacer_width)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getDrawableForWifiStandard(): Int {
+        return when (currWifiStandard) {
+            4 -> R.drawable.ic_wifi_standard_4
+            5 -> R.drawable.ic_wifi_standard_5
+            6 -> R.drawable.ic_wifi_standard_6
+            7 -> R.drawable.ic_wifi_standard_7
+            else -> 0
+        }
+    }
+}
