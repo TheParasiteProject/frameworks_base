2ffcac548f55ff41d53fde7ada80def61ea383c8
Bo Liang <ot_bo.liang@mediatek.com>
2023-12-11 15:39:16 +0800

    Fix screen can not be switched issue
    This is a screen can not be switched issue for Foldable Phone, When switched screen by cmd "adb shell cmd device_state state X" to change screen from state 0 to 1, The screen can not switch success from secondary screen to main screen, because after LogicalDisplayMapper sending swapped event, the main screen power_state will never changed.
    
    We add update power mode when DMS received Swapped event, like this, DMS will update power mode ON to SF, and screen can be switched success.
    
    Bug: 356561060
    Test: Screen be switched success
    
    Change-Id: I1fceb23c09215091f0d18c4c8bc0368edfbd24db

--- services/core/java/com/android/server/display/DisplayManagerService.java
+++ services/core/java/com/android/server/display/DisplayManagerService.java
@@ -2293,6 +2293,8 @@ public final class DisplayManagerService extends SystemService {
     private void handleLogicalDisplaySwappedLocked(@NonNull LogicalDisplay display) {
         handleLogicalDisplayChangedLocked(display);
 
+        updateLogicalDisplayState(display);
+
         final int displayId = display.getDisplayIdLocked();
         if (displayId == Display.DEFAULT_DISPLAY) {
             notifyDefaultDisplayDeviceUpdated(display);
