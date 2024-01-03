b64bc7d0fa45ee16897c054ec731316702bc0c55
Pranav Vashi <neobuddy89@gmail.com>
2024-01-04 00:10:32 +0530

    ActivityManagerService: Exclude more packages for receiver check [1/2]
    * ASI errors when adding Now Playing shortcut on screen.
    
    [someone5678]
    * Moved to PixelPropsUtils
    * Use RRO
    
    Initial package lists:
    com.android.systemui
    com.google.android.as
    com.google.android.gms
    
    Change-Id: I303a2b2637f2fe424510ebee8156a8df751e5656
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

--- services/core/java/com/android/server/am/BroadcastController.java
+++ services/core/java/com/android/server/am/BroadcastController.java
@@ -136,6 +136,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 
+import com.android.internal.util.custom.PixelPropsUtils;
+
 class BroadcastController {
     private static final String TAG_BROADCAST = TAG + POSTFIX_BROADCAST;
 
@@ -473,7 +475,8 @@ class BroadcastController {
             if (receiver == null && !explicitExportStateDefined) {
                 // sticky broadcast, no flag specified (flag isn't required)
                 flags |= Context.RECEIVER_EXPORTED;
-            } else if (requireExplicitFlagForDynamicReceivers && !explicitExportStateDefined) {
+            } else if (requireExplicitFlagForDynamicReceivers && !explicitExportStateDefined
+                    && !PixelPropsUtils.shouldBypassBroadcastReceiverValidation(callerPackage)) {
                 throw new SecurityException(
                         callerPackage + ": One of RECEIVER_EXPORTED or "
                                 + "RECEIVER_NOT_EXPORTED should be specified when a receiver "
