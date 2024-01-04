6a94b292e28248d1d3d18b2cc0e2b38d601d1927
someone5678 <someone5678@users.noreply.github.com>
2024-01-04 17:49:17 +0900

    PixelPropsUtils: Check process name before doing certify
    Change-Id: I34494330ac4aa1a729e9897e2cdd5eb70d7b2d0d

--- core/java/com/android/internal/util/custom/PixelPropsUtils.java
+++ core/java/com/android/internal/util/custom/PixelPropsUtils.java
@@ -198,6 +198,13 @@ public class PixelPropsUtils {
     private static boolean shouldTryToCertifyDevice() {
         if (!sIsGms) return false;
 
+        final String processName = Application.getProcessName();
+        if (!processName.toLowerCase().contains("unstable")
+                && !processName.toLowerCase().contains("pixelmigrate")
+                && !processName.toLowerCase().contains("instrumentation")) {
+            return false;
+        }
+
         final boolean[] shouldCertify = {true};
 
         setPropValue("TIME", System.currentTimeMillis());
