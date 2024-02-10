2a9616552b81f575b41de03d1c191c7c32b10dea
someone5678 <someone5678@users.noreply.github.com>
2024-02-10 17:15:43 +0900

    base: Use Context instead of Application for *PropsUtils [2/2]
    Change-Id: Ie6d4b019a5be1dccb9d1c554c6ec97ed0b2fa330

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -1363,9 +1363,9 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PixelPropsUtils.setProps(app);
-        GamesPropsUtils.setProps(app);
-        MeizuPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(context);
+        GamesPropsUtils.setProps(context);
+        MeizuPropsUtils.setProps(context);
         return app;
     }
 
@@ -1384,9 +1384,9 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PixelPropsUtils.setProps(app);
-        GamesPropsUtils.setProps(app);
-        MeizuPropsUtils.setProps(app);
+        PixelPropsUtils.setProps(context);
+        GamesPropsUtils.setProps(context);
+        MeizuPropsUtils.setProps(context);
         return app;
     }
 
