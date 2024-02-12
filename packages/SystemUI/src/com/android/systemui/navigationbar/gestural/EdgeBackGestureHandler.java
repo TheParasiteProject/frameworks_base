66b2ea19a7e86f0ec9aaa4abe42a04c31c32d1ef
someone5678 <someone5678@users.noreply.github.com>
2024-02-13 06:16:15 +0900

    Gesture nav: Add go forward action
    * Used those for web browser
    
    Change-Id: Id9a37d5f9f08cc682ac82d212fb09bda8ce7b1af
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -1373,6 +1373,9 @@ public class EdgeBackGestureHandler implements PluginListener<NavigationEdgeBack
             case 16: // Recents
                 CustomUtils.sendKeycode(KeyEvent.KEYCODE_APP_SWITCH);
                 break;
+            case 17: // Go forward
+                CustomUtils.sendKeycode(KeyEvent.KEYCODE_FORWARD);
+                break;
         }
     }
 
