0347efe2f483ec2f7ff50d8e8302b480877e510b
someone5678 <someone5678@users.noreply.github.com>
2024-03-07 21:02:14 +0900

    ApplicationPackageManager: Import CustomFeaturesUtils changes
    Change-Id: Ic207d106a913dd5b8f633841632f296a9e821e3b

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -135,6 +135,7 @@ import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.os.SomeArgs;
 import com.android.internal.pm.RoSystemFeatures;
 import com.android.internal.util.UserIcons;
+import com.android.internal.util.custom.CustomFeaturesUtils;
 
 import com.nvidia.NvAppProfileService;
 
@@ -839,6 +840,12 @@ public class ApplicationPackageManager extends PackageManager {
 
     @Override
     public boolean hasSystemFeature(String name, int version) {
+        final int report = CustomFeaturesUtils.hasSystemFeatureCustom(name);
+        switch (report) {
+            case 0: return false;
+            case 1: return true;
+            default:
+        }
         // We check for system features in the following order:
         //    * Build time-defined system features (constant, very efficient)
         //    * SDK-defined system features (cached at process start, very efficient)
