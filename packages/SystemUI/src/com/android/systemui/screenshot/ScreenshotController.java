e4959e52eb819b95d78212dc6118ccf81efe2e3e
Michael Bestas <mkbestas@lineageos.org>
2024-03-10 21:54:58 +0200

    fixup! SystemUI: screenshot: open long screenshot activity for partial screenshots
    Change-Id: I24f4bf47f808cedc96dda70b133fe2b472bebd4b

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -853,9 +853,9 @@ public class ScreenshotController {
 
     private void startPartialScreenshotActivity(UserHandle owner) {
         DisplayMetrics displayMetrics = new DisplayMetrics();
-        getDefaultDisplay().getRealMetrics(displayMetrics);
+        getDisplay().getRealMetrics(displayMetrics);
 
-        Bitmap newScreenshot = mImageCapture.captureDisplay(mDisplayTracker.getDefaultDisplayId(),
+        Bitmap newScreenshot = mImageCapture.captureDisplay(mDisplayId,
                 new Rect(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels));
         ScrollCaptureController.BitmapScreenshot bitmapScreenshot =
                 new ScrollCaptureController.BitmapScreenshot(mContext, newScreenshot);
