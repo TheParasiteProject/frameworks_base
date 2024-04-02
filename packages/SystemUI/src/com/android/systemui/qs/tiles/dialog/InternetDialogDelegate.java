77fd41142627924fe82011faae272a0bf1c911c6
someone5678 <someone5678@users.noreply.github.com>
2024-04-03 02:12:16 +0900

    SystemUI: Launch hotspot settings on long click instead of click
    * Follow other toggle behavior of QS internet dialog for consistency
    
    Change-Id: I9fc1958bd49b22da7daf4abf83db512d49ce81df

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegate.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialogDelegate.java
@@ -327,7 +327,7 @@ public class InternetDialogDelegate implements
         }
         mMobileNetworkLayout.setOnClickListener(null);
         mMobileNetworkLayout.setOnLongClickListener(null);
-        mHotspotLayout.setOnClickListener(null);
+        mHotspotLayout.setOnLongClickListener(null);
         mHotspotToggle.setOnCheckedChangeListener(null);
         mConnectedWifListLayout.setOnClickListener(null);
         if (mSecondaryMobileNetworkLayout != null) {
@@ -420,7 +420,10 @@ public class InternetDialogDelegate implements
                         dialog.getContext(), mDefaultDataSubId, isChecked, false);
             }
         });
-        mHotspotLayout.setOnClickListener(mInternetDialogController::launchHotspotSetting);
+        mHotspotLayout.setOnLongClickListener(v -> {
+                mInternetDialogController.launchHotspotSetting(v);
+                return true;
+        });
         mHotspotToggle.setOnCheckedChangeListener(
                 (buttonView, isChecked) -> mInternetDialogController.setHotspotEnabled(isChecked));
         mConnectedWifListLayout.setOnClickListener(this::onClickConnectedWifi);
