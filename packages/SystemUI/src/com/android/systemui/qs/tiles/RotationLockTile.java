02113d89eefb02d0f75e5d6ee9bdc66d18dcd352
someone5678 <someone5678@users.noreply.github.com>
2024-05-18 08:29:11 -0400

    Revert "RotationLockTile: Open LineageParts rotation settings"
    * We will directly add rotation settings to Settings app
      instead of LineageParts
    
    This reverts commit f8548bc7e9251f02daa375f03248a178a30a29f9.
    
    Change-Id: I86cef9421413f6ece1388c1f94d1e2e37d6e0ee6

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RotationLockTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RotationLockTile.java
@@ -63,9 +63,6 @@ public class RotationLockTile extends QSTileImpl<BooleanState> implements
 
     private static final String EMPTY_SECONDARY_STRING = "";
 
-    private static final String ROTATION_SETTINGS =
-            "org.lineageos.lineageparts.DISPLAY_ROTATION";
-
     private final Icon mIcon = ResourceIcon.get(com.android.internal.R.drawable.ic_qs_auto_rotate);
     private final RotationLockController mController;
     private final SensorPrivacyManager mPrivacyManager;
@@ -130,7 +127,7 @@ public class RotationLockTile extends QSTileImpl<BooleanState> implements
 
     @Override
     public Intent getLongClickIntent() {
-        return new Intent(ROTATION_SETTINGS);
+        return new Intent(Settings.ACTION_AUTO_ROTATE_SETTINGS);
     }
 
     @Override
