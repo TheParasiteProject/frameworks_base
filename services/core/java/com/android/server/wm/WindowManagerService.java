8333227cd9601c5d9ef9dab7051f8531efa4649a
someone5678 <someone5678@users.noreply.github.com>
2024-05-21 09:55:51 -0400

    base: Allow to hide screen capture status from apps [1/2]
    * Allow to hide screen capture detection from privacy invasive apps
    
    Change-Id: Ie2a8e49614dcd8fce2311a644d615ab60670c094
    Signed-off-by: someone5678 <someone5678@users.noreply.github.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -10753,6 +10753,7 @@ public class WindowManagerService extends IWindowManager.Stub
             throw new SecurityException("Requires STATUS_BAR_SERVICE permission");
         }
         synchronized (mGlobalLock) {
+            if (shouldHideScreenCapture()) return new ArrayList<>();
             final DisplayContent displayContent = mRoot.getDisplayContent(displayId);
             if (displayContent == null) {
                 return new ArrayList<>();
@@ -10854,6 +10855,10 @@ public class WindowManagerService extends IWindowManager.Stub
     @EnforcePermission(android.Manifest.permission.DETECT_SCREEN_RECORDING)
     @Override
     public boolean registerScreenRecordingCallback(IScreenRecordingCallback callback) {
+        if (shouldHideScreenCapture()) {
+            return false;
+        }
+
         registerScreenRecordingCallback_enforcePermission();
         return mScreenRecordingCallbackController.register(callback);
     }
@@ -10866,6 +10871,10 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     void onProcessActivityVisibilityChanged(int uid, boolean visible) {
+        if (shouldHideScreenCapture()) {
+            return;
+        }
+
         mScreenRecordingCallbackController.onProcessActivityVisibilityChanged(uid, visible);
     }
 
@@ -11002,6 +11011,11 @@ public class WindowManagerService extends IWindowManager.Stub
     @Override
     public void performKeyActionFromIntSafe(int actionCode) {
         mPolicy.performKeyActionFromIntSafe(actionCode);
+    }
+
+    private boolean shouldHideScreenCapture() {
+        return Settings.Global.getInt(mContext.getContentResolver(),
+                Settings.Global.HIDE_SCREEN_CAPTURE_STATUS, 0) != 0;
     }
 
 }
