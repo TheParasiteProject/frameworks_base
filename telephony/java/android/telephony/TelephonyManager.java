213ba46b47f4d5d759b833bfef97a5684e955d38
minaripenguin <minaripenguin@users.noreply.github.com>
2024-05-30 22:36:33 +0800

    TelephonyManager: Gracefully handle null telephony service [2]
    --------- beginning of crash
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: FATAL EXCEPTION: pool-2-thread-1
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: Process: com.google.android.connectivitythermalpowermanager, PID: 7184
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: java.lang.IllegalStateException: telephony service is null.
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at android.telephony.TelephonyManager.getAllowedNetworkTypesForReason(TelephonyManager.java:9681)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at com.google.android.connectivitythermalpowermanager.power.cellular.decision.PowerManagerDecision.internalApplyCriteriaAllowedNetworkTypes(PowerManagerDecision.java:928)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at com.google.android.connectivitythermalpowermanager.power.cellular.decision.PowerManagerDecision.applyCriteriaAllowedNetworkTypesWithRetry(PowerManagerDecision.java:888)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at com.google.android.connectivitythermalpowermanager.power.cellular.decision.PowerManagerDecision.lambda$applyCriteriaAllowedNetworkTypes$4(PowerManagerDecision.java:905)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at com.google.android.connectivitythermalpowermanager.power.cellular.decision.PowerManagerDecision.$r8$lambda$YOnForDFleFGD2Y5mI1VezN2WgU(PowerManagerDecision.java:0)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at com.google.android.connectivitythermalpowermanager.power.cellular.decision.PowerManagerDecision$$ExternalSyntheticLambda7.run(R8$$SyntheticClass:0)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1145)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:644)
    05-30 17:19:07.775  7184  7418 E AndroidRuntime: 	at java.lang.Thread.run(Thread.java:1012)
    
    reference:
    https://github.com/RisingOS-staging/android_packages_apps_Settings/blob/1e59ca72a0d3a67e86be2d96ec6486189beca50d/src/com/android/settings/network/telephony/cdma/CdmaSystemSelectPreferenceController.java#L69
    https://github.com/RisingOS-staging/android_packages_apps_Settings/blob/1e59ca72a0d3a67e86be2d96ec6486189beca50d/src/com/android/settings/network/telephony/PreferredNetworkModePreferenceController.java#L106
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -10114,7 +10114,8 @@ public class TelephonyManager {
             if (telephony != null) {
                 return telephony.getAllowedNetworkTypesForReason(getSubId(), reason);
             } else {
-                throw new IllegalStateException("telephony service is null.");
+                Rlog.d(TAG, "telephony service is null.");
+                return -1; // NETWORK_MODE_UNKNOWN
             }
         } catch (RemoteException ex) {
             Rlog.e(TAG, "getAllowedNetworkTypesForReason RemoteException", ex);
