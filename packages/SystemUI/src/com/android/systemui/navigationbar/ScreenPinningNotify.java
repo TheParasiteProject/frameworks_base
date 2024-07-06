75ee0529df8e35fe383c4000658da84204ac2f18
someone5678 <someone5678@users.noreply.github.com>
2024-07-06 12:34:21 +0900

    Revert "Fix screen unpinning text on devices that have gestures on fp sensor"
    This reverts commit 23b2f609492218e7111d763062b6e0d11a599a1c.
    
    Change-Id: I76c655313a273fcc7415a496fe9d56cd1f8122f0

--- packages/SystemUI/src/com/android/systemui/navigationbar/ScreenPinningNotify.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/ScreenPinningNotify.java
@@ -66,11 +66,9 @@ public class ScreenPinningNotify {
         if (mLastToast != null) {
             mLastToast.cancel();
         }
-        int noNavbarResId = supportsGesturesOnFP() ?
-                R.string.screen_pinning_toast_no_navbar_fpsensor :
-                R.string.screen_pinning_toast_no_navbar;
         mLastToast = makeAllUserToastAndShow(!hasNavigationBar()
-                ? noNavbarResId : (isGestureNavEnabled
+                ? R.string.screen_pinning_toast_no_navbar
+                : (isGestureNavEnabled
                 ? R.string.screen_pinning_toast_gesture_nav
                 : isRecentsButtonVisible
                         ? R.string.screen_pinning_toast
@@ -91,10 +89,5 @@ public class ScreenPinningNotify {
             // ignore
         }
         return false;
-    }
-
-    private boolean supportsGesturesOnFP() {
-        return mContext.getResources().getBoolean(
-            org.lineageos.platform.internal.R.bool.config_supportsGesturesOnFingerprintSensor);
-    }
+     }
 }
