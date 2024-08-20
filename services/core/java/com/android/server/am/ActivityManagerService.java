15b467e103e5e84eb111668f97cc301e7cc83529
Pranav Vashi <neobuddy89@gmail.com>
2024-08-21 00:28:48 +0530

    Add three fingers swipe actions [2/3]
    * Inspired by: https://github.com/RisingTechOSS/android_frameworks_base/commit/02780e27862edfa7c0b957073e9f4e1e4da2fb7d
    * Listener originally authored by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Change-Id: Ia2dcbbbbd6ebcac1d226145a96d389d79272cadd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1579,6 +1579,9 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     static final HostingRecord sNullHostingRecord =
             new HostingRecord(HostingRecord.HOSTING_TYPE_EMPTY);
+
+    private boolean mThreeFingersSwipeEnabled;
+
     /**
      * Used to notify activity lifecycle events.
      */
@@ -19088,5 +19091,21 @@ public class ActivityManagerService extends IActivityManager.Stub
     public boolean shouldForceLongScreen(String packageName) {
         return mActivityTaskManager.shouldForceLongScreen(packageName);
     }
-    
+
+    @Override
+    public boolean isThreeFingersSwipeActive() {
+        if (!mThreeFingersSwipeEnabled)
+            return false;
+        synchronized (this) {
+            return Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.THREE_FINGER_GESTURE_ACTIVE, 0,
+                    UserHandle.USER_CURRENT) == 1;
+        }
+    }
+
+    @Override
+    public void setThreeFingersSwipeActive(boolean active) {
+        mThreeFingersSwipeEnabled = active;
+    }
+
 }
