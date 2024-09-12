186444c99c02fda356618cb022d4808570cd2ad8
minaripenguin <minaripenguin@users.noreply.github.com>
2024-09-12 22:09:13 +0800

    fixup! MobileSignalController: Prevent possible memory leak
    Fixup for:
    SystemUI: Make roaming indicator optional [1/2]
    SystemUI: Allow using 4G icon instead of LTE [1/2]
    SystemUI: Option to disable Data Disabled Indicator icon [1/2]
    
    Change-Id: I0c65148953b117ece71226471e654fc3b624fe66

--- packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/connectivity/MobileSignalController.java
@@ -91,6 +91,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     boolean mInflateSignalStrengths = false;
     @VisibleForTesting
     final MobileStatusTracker mMobileStatusTracker;
+    private final TunerService mTunerService;
 
     // Save the previous STATUS_HISTORY_SIZE states for logging.
     private final String[] mMobileStatusHistory = new String[STATUS_HISTORY_SIZE];
@@ -174,9 +175,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         };
         mMobileStatusTracker = mobileStatusTrackerFactory.createTracker(mMobileCallback);
 
-        Dependency.get(TunerService.class).addTunable(this, DATA_DISABLED_ICON);
-        Dependency.get(TunerService.class).addTunable(this, SHOW_FOURG_ICON);
-        Dependency.get(TunerService.class).addTunable(this, ROAMING_INDICATOR_ICON);
+        mTunerService = Dependency.get(TunerService.class);
     }
 
     @Override
@@ -244,6 +243,9 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
         mContext.getContentResolver().registerContentObserver(Global.getUriFor(
                 Global.MOBILE_DATA + mSubscriptionInfo.getSubscriptionId()),
                 true, mObserver);
+        mTunerService.addTunable(this, DATA_DISABLED_ICON);
+        mTunerService.addTunable(this, SHOW_FOURG_ICON);
+        mTunerService.addTunable(this, ROAMING_INDICATOR_ICON);
     }
 
     /**
@@ -252,6 +254,7 @@ public class MobileSignalController extends SignalController<MobileState, Mobile
     public void unregisterListener() {
         mMobileStatusTracker.setListening(false);
         mContext.getContentResolver().unregisterContentObserver(mObserver);
+        mTunerService.removeTunable(this);
     }
 
     private void updateInflateSignalStrength() {
