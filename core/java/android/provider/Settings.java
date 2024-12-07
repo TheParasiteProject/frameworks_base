c1e08e9d3ee998634052f623aff163b94024f270
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-07 10:14:03 +0900

    base: Add support for hide applist [1/3]
    * Based on commit "Hide hidden apps from all apps except system"
    * Hide user selected apps from applist
    * The applist includes system apps (including overlays), and user apps
    
    Ref:
    https://review.calyxos.org/c/CalyxOS/platform_frameworks_base/+/16571
    https://github.com/TheParasiteProject/frameworks_base/commit/6c41a0fc2c7ba01fd81132ceb0f3c1476099cf25
    https://github.com/LineageOS/android_lineage-sdk/blob/lineage-16.0/sdk/src/java/org/lineageos/internal/applications/LongScreen.java
    https://github.com/yaap/frameworks_base/commit/232260f192effc9fb21a54eb444601179b24a26d
    
    Change-Id: If6c94fd345c488edab11927c4f567bc2171f69b2
    Signed-off-by: someone5678 <59456192+someone5678@users.noreply.github.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -12688,6 +12688,13 @@ public final class Settings {
         public static final String HIDE_DEVELOPER_STATUS = "hide_developer_status";
 
         /**
+         * Control which apps to hide from other user apps.
+         * @hide
+         */
+        @Readable
+        public static final String HIDE_APPLIST = "hide_applist";
+
+        /**
          * Whether to enable clipboard auto clear
          * @hide
          */
