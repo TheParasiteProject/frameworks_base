76240a8dbe4f9d36c3bab6b20f6d3f30a53568c0
someone5678 <59456192+someone5678@users.noreply.github.com>
2024-12-08 19:09:03 +0900

    fixup! base: Hide ADB and developer setting enable status [1/2]

--- core/java/com/android/internal/util/custom/HideDeveloperStatusUtils.java
+++ core/java/com/android/internal/util/custom/HideDeveloperStatusUtils.java
@@ -2,22 +2,20 @@ package com.android.internal.util.custom;
 
 import android.content.ContentResolver;
 import android.content.Context;
-import android.os.UserHandle;
+import android.os.SystemProperties;
+import android.provider.Settings;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-import android.provider.Settings;
-
 public class HideDeveloperStatusUtils {
     private static final Set<String> settingsToHide =
-        new HashSet<>(
-            Arrays.asList(
-                Settings.Global.ADB_ENABLED,
-                Settings.Global.ADB_WIFI_ENABLED,
-                Settings.Global.DEVELOPMENT_SETTINGS_ENABLED
-            ));
+            new HashSet<>(
+                    Arrays.asList(
+                            Settings.Global.ADB_ENABLED,
+                            Settings.Global.ADB_WIFI_ENABLED,
+                            Settings.Global.DEVELOPMENT_SETTINGS_ENABLED));
 
     enum Action {
         ADD,
@@ -25,9 +23,12 @@ public class HideDeveloperStatusUtils {
         SET
     }
 
-    public static boolean shouldHideDevStatus(
-            ContentResolver cr, String packageName, String name) {
-        if (cr == null || packageName == null || name == null) {
+    private static boolean isBootCompleted() {
+        return SystemProperties.getBoolean("sys.boot_completed", false);
+    }
+
+    public static boolean shouldHideDevStatus(ContentResolver cr, String packageName, String name) {
+        if (cr == null || packageName == null || name == null || !isBootCompleted()) {
             return false;
         }
 
@@ -61,8 +62,7 @@ public class HideDeveloperStatusUtils {
     }
 
     private static void putAppsForUser(
-            Context context, String packageName,
-            int userId, Action action) {
+            Context context, String packageName, int userId, Action action) {
         if (context == null || userId < 0) {
             return;
         }
@@ -80,8 +80,11 @@ public class HideDeveloperStatusUtils {
                 break;
         }
 
-        Settings.Secure.putStringForUser(context.getContentResolver(),
-                Settings.Secure.HIDE_DEVELOPER_STATUS, String.join(",", apps), userId);
+        Settings.Secure.putStringForUser(
+                context.getContentResolver(),
+                Settings.Secure.HIDE_DEVELOPER_STATUS,
+                String.join(",", apps),
+                userId);
     }
 
     public void addApp(Context mContext, String packageName, int userId) {
