0060373f2538ec486684fac583c2731547908ae7
LuK1337 <priv.luk@gmail.com>
2024-12-28 15:41:01 +0100

    SystemUI: Fix constraints for keyguard slice view
    With this, the clock and date are aligned with each other properly on
    tablets.
    
    Change-Id: Ib0954182892501cddcb6988a6afc17f8c4208d6d

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/view/layout/sections/KeyguardSliceViewSection.kt
@@ -17,6 +17,7 @@
 
 package com.android.systemui.keyguard.ui.view.layout.sections
 
+import android.content.Context
 import android.os.Handler
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.Barrier
@@ -24,6 +25,7 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import com.android.keyguard.KeyguardSliceView
 import com.android.keyguard.KeyguardSliceViewController
+import com.android.systemui.customization.clocks.R as clocksR
 import com.android.systemui.dagger.qualifiers.Background
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.dump.DumpManager
@@ -43,6 +45,7 @@ import kotlinx.coroutines.DisposableHandle
 class KeyguardSliceViewSection
 @Inject
 constructor(
+    private val context: Context,
     val smartspaceController: LockscreenSmartspaceController,
     val layoutInflater: LayoutInflater,
     @Main val handler: Handler,
@@ -97,6 +100,8 @@ constructor(
                 ConstraintSet.START,
                 ConstraintSet.PARENT_ID,
                 ConstraintSet.START,
+                context.resources.getDimensionPixelSize(clocksR.dimen.clock_padding_start) +
+                    context.resources.getDimensionPixelSize(clocksR.dimen.status_view_margin_horizontal),
             )
             connect(
                 R.id.keyguard_slice_view,
