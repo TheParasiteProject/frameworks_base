5a2ffc3dcea16ba55d530fbbc762f6f1bc21506a
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-01-04 03:19:52 +0900

    base: Add grantDefaultPermissionsToCustomPackages
    * For organizing
    
    Change-Id: I1a41b86ebe1595f543c135242937374797f9f3e9

--- services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -932,6 +932,8 @@ final class DefaultPermissionGrantPolicy {
         String commonServiceAction = "android.adservices.AD_SERVICES_COMMON_SERVICE";
         grantPermissionsToSystemPackage(pm, getDefaultSystemHandlerServicePackage(pm,
                         commonServiceAction, userId), userId, NOTIFICATION_PERMISSIONS);
+        
+        grantDefaultPermissionsToCustomPackages(pm, userId);
     }
 
     private String getDefaultSystemHandlerActivityPackageForCategory(PackageManagerWrapper pm,
@@ -2019,4 +2021,9 @@ final class DefaultPermissionGrantPolicy {
             this.whitelisted = whitelisted;
         }
     }
+
+    private void grantDefaultPermissionsToCustomPackages(PackageManagerWrapper pm, int userId) {
+        
+    }
+
 }
