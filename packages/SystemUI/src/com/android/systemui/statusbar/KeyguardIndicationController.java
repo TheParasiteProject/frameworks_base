2649210d14688e24f2f07431d784491f9e072379
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-24 08:36:07 +0800

    SystemUI: Show Battery Defender indication only when battery level reaches charging threshold
    Change-Id: I40011190d3d9ad5264afdde5bd0720bb60ee38fe
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -1376,7 +1376,7 @@ public class KeyguardIndicationController {
             mChargingStatus = status.chargingStatus;
             mBatteryLevel = status.level;
             mBatteryPresent = status.present;
-            mBatteryDefender = isBatteryDefender(status);
+            mBatteryDefender = isBatteryDefender(status) && mBatteryLevel >= 80;
             mBatteryDead = status.isDead();
             // when the battery is overheated, device doesn't charge so only guard on pluggedIn:
             mEnableBatteryDefender = mBatteryDefender && status.isPluggedIn();
