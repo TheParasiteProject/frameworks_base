35f4237c7e1f4b8de5821544758406bb238bb090
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-24 09:24:15 +0800

    DisplayModeDirector: Set default minimum refresh rate to 60
    matches new refresh rate selector
    
    Change-Id: I0c9b775a8190937536510df42759242df186bf81

--- services/core/java/com/android/server/display/mode/DisplayModeDirector.java
+++ services/core/java/com/android/server/display/mode/DisplayModeDirector.java
@@ -1201,7 +1201,7 @@ public class DisplayModeDirector {
             float highestRefreshRate = getMaxRefreshRateLocked(displayId);
 
             float minRefreshRate = Settings.System.getFloatForUser(cr,
-                    Settings.System.MIN_REFRESH_RATE, 0f, UserHandle.USER_CURRENT);
+                    Settings.System.MIN_REFRESH_RATE, 60f, UserHandle.USER_CURRENT);
             if (Float.isInfinite(minRefreshRate)) {
                 // Infinity means that we want the highest possible refresh rate
                 minRefreshRate = highestRefreshRate;
