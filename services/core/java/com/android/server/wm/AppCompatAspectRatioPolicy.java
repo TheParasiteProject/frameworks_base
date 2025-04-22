b3e664d40fea2b984a87627494c5cb33fac4d64c
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-23 07:56:03 +0800

    services: improve cutout force full screen
    reverse-engineered from nothing os 3.0
    
    Change-Id: I750c52383d817e0697ed7ee6d0e7f1ca2704ba10
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/wm/AppCompatAspectRatioPolicy.java
+++ services/core/java/com/android/server/wm/AppCompatAspectRatioPolicy.java
@@ -394,6 +394,9 @@ class AppCompatAspectRatioPolicy {
             top = containingBounds.top;
         }
         outBounds.set(left, top, right, bottom);
+        if (mActivityRecord.shouldForceLongScreen()) {
+            outBounds.set(mActivityRecord.getRequestedOverrideBounds());
+        }
         return true;
     }
 
