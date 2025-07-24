1eacfbd48a31548bcb1c603ac217eea87117b15d
Joey Huab <jhuab@edukasyon.ph>
2025-07-24 10:28:44 +0900

    Revert "Biometrics: Hook up support for halHandlesDisplayTouches prop"
    * Breaks UDFPS for some tensor devices.
    
    This reverts commit 20ad479f7e02990a497f41c2270b7c15eb57c48f.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/biometrics/AuthService.java
+++ services/core/java/com/android/server/biometrics/AuthService.java
@@ -1114,7 +1114,6 @@ public class AuthService extends SystemService {
             return new FingerprintSensorPropertiesInternal(sensorId,
                     Utils.authenticatorStrengthToPropertyStrength(strength), maxEnrollmentsPerUser,
                     componentInfo, sensorType, true /* halControlsIllumination */,
-                    false /* halHandlesDisplayTouches */,
                     resetLockoutRequiresHardwareAuthToken,
                     List.of(new SensorLocationInternal("" /* display */, udfpsProps[0],
                             udfpsProps[1], udfpsProps[2])));
