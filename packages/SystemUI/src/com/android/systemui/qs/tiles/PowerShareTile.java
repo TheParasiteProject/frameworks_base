4a7e98babb3507f10e2cf46ed1f7c51b69d73272
LuK1337 <priv.luk@gmail.com>
2025-08-23 13:41:24 +0200

    PowerShareTile: Mark is unavailable if enabled state can't be read
    Change-Id: I6e043a9fd4e440377691f94ca83946f3e213e672

--- packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
@@ -143,7 +143,13 @@ public class PowerShareTile extends QSTileImpl<BooleanState>
 
     @Override
     public boolean isAvailable() {
-        return mPowerShare != null;
+        try {
+            // Will throw NPE or some other exception if HAL is either missing or broken.
+            mPowerShare.isEnabled();
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
     @Override
