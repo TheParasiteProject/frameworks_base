440ded2981d3ba7f72ec4dd2ec3e5aa39700df0c
bijoyv9 <bijoyv9@gmail.com>
2025-08-24 18:35:50 +0000

    SystemUI: Prevent keyguard state conflicts that caused screen flickering
    Change-Id: I93a19e72d9b6f96394875e5e4da0f89bc522ed2c

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -122,6 +122,7 @@ import com.android.internal.policy.IKeyguardService;
 import com.android.internal.policy.IKeyguardStateCallback;
 import com.android.internal.policy.ScreenDecorationsUtils;
 import com.android.internal.statusbar.IStatusBarService;
+import com.android.internal.util.ArrayUtils;
 import com.android.internal.util.LatencyTracker;
 import com.android.internal.widget.LockPatternUtils;
 import com.android.keyguard.KeyguardConstants;
@@ -1860,6 +1861,12 @@ public class KeyguardViewMediator implements CoreStartable,
         maybeSendUserPresentBroadcast();
     }
 
+    private boolean isUdfpsConfigured() {
+        int[] udfpsProps = mContext.getResources().getIntArray(
+            com.android.internal.R.array.config_udfps_sensor_props);
+        return !ArrayUtils.isEmpty(udfpsProps);
+    }
+
     /**
      * Called to let us know the screen was turned off.
      * @param offReason either {@link WindowManagerPolicyConstants#OFF_BECAUSE_OF_USER} or
@@ -1904,12 +1911,16 @@ public class KeyguardViewMediator implements CoreStartable,
 
         mUpdateMonitor.dispatchStartedGoingToSleep(offReason);
 
-        // Reset keyguard going away state, so we can start listening for fingerprint. We
-        // explicitly DO NOT want to call
-        // mKeyguardViewControllerLazy.get().setKeyguardGoingAwayState(false)
-        // here, since that will mess with the device lock state.
-        mKeyguardStateController.notifyKeyguardGoingAway(false);
-        mUpdateMonitor.dispatchKeyguardGoingAway(false);
+        // Prevent keyguard going away conflicts during lock operations
+        boolean isLockingOperation = mShowing || mKeyguardStateController.isKeyguardGoingAway() || mPendingLock;
+    
+        // Only dispatch keyguard going away if:
+        // 1. UDFPS is not configured (original logic)
+        // 2. Not currently in a locking operation
+        if (!isUdfpsConfigured() && !isLockingOperation) {
+            mKeyguardStateController.notifyKeyguardGoingAway(false);
+            mUpdateMonitor.dispatchKeyguardGoingAway(false);
+        }
 
         notifyStartedGoingToSleep();
     }
