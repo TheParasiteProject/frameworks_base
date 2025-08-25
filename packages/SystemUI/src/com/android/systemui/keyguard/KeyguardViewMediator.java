8fed0ff792d0f4daff2c576125209020fe34f96d
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-08-25 13:05:06 +0800

    [BUGFIX] udfps flicker fix enhancement
    the patch should be applied only when udfps is enrolled, because the issue only happens on udfps unlock
    
    Change-Id: I5d9d83d2d59aab03a36d1618b4e159b7d7d92539
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1861,12 +1861,6 @@ public class KeyguardViewMediator implements CoreStartable,
         maybeSendUserPresentBroadcast();
     }
 
-    private boolean isUdfpsConfigured() {
-        int[] udfpsProps = mContext.getResources().getIntArray(
-            com.android.internal.R.array.config_udfps_sensor_props);
-        return !ArrayUtils.isEmpty(udfpsProps);
-    }
-
     /**
      * Called to let us know the screen was turned off.
      * @param offReason either {@link WindowManagerPolicyConstants#OFF_BECAUSE_OF_USER} or
@@ -1917,7 +1911,7 @@ public class KeyguardViewMediator implements CoreStartable,
         // Only dispatch keyguard going away if:
         // 1. UDFPS is not configured (original logic)
         // 2. Not currently in a locking operation
-        if (!isUdfpsConfigured() && !isLockingOperation) {
+        if (!mUpdateMonitor.isUdfpsEnrolled() && !isLockingOperation) {
             mKeyguardStateController.notifyKeyguardGoingAway(false);
             mUpdateMonitor.dispatchKeyguardGoingAway(false);
         }
