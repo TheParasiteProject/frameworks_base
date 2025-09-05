e154d3c5a75fa6f6f5ed698ae30d8284c3540728
Pranav Vashi <neobuddy89@gmail.com>
2025-09-05 20:39:12 +0530

    core: Make CHARGE_OPTIMIZATION_MODE readable for pixel agent
    Log:
    
    09-05 20:33:49.029 D/AndroidRuntime(29307): Shutting down VM
    09-05 20:33:49.030 E/AndroidRuntime(29307): FATAL EXCEPTION: main
    09-05 20:33:49.030 E/AndroidRuntime(29307): Process: com.google.android.apps.pixel.agent, PID: 29307
    09-05 20:33:49.030 E/AndroidRuntime(29307): java.lang.SecurityException: Settings key: <charge_optimization_mode> is not readable. From S+, settings keys annotated with @\hide are restricted to system_server and system apps only, unless they are annotated with @\Readable.
    
    Change-Id: I2b16daef4ec602d7d4dbe2333dc264dc72a5320c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14168,6 +14168,7 @@ public final class Settings {
          *
          * @hide
          */
+        @Readable
         public static final String CHARGE_OPTIMIZATION_MODE = "charge_optimization_mode";
 
         /**
