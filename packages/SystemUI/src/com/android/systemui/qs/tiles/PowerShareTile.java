00718f07b562fa3a5a6260604ff727cedc73ea25
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-09-22 16:11:57 +0900

    fixup! SystemUI: Add powershare QS tile
    * Unregister BatteryController callback on destroy

--- packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/PowerShareTile.java
@@ -213,6 +213,12 @@ public class PowerShareTile extends QSTileImpl<BooleanState>
     }
 
     @Override
+    public void destroy() {
+        mBatteryController.removeCallback(this);
+        super.destroy();
+    }
+
+    @Override
     public void handleSetListening(boolean listening) {
     }
 
